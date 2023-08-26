7c24d9fc7b1d9e2281922e439f982e7ba0082c52
Gaurav Chavhan <121816957+gaurav07220@users.noreply.github.com>
2023-08-26 18:14:21 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
     <BrowserRouter basename="">
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/gaurav-portfolio" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
